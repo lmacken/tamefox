18f5df63befb076d04a19220af8d8c4731e5b475
Luke Macken <lmacken@redhat.com>
2011-09-21 12:59:19 -0400

    Revert "Revert "Put the entire process group to sleep.""
    This reverts commit da40370422b5d57c8258295060adb92f1156cec7.

--- tamefox.py
+++ tamefox.py
@@ -64,18 +64,20 @@ def tamefox():
     """ Puts firefox to sleep when it loses focus """
     alive = True
     ff_pid = None
+    pgid = None
     for property, title, pid, event, parent in watch(['_NET_ACTIVE_WINDOW']):
         if parent in TAME:
             ff_pid = pid
+            pgid = os.getpgid(pid)
             if not alive:
                 print 'Waking up firefox'
-                os.kill(ff_pid, SIGCONT)
+                os.killpg(pgid, SIGCONT)
                 alive = True
         elif ff_pid and alive:
             print 'Putting firefox to sleep'
             dpy.grab_server()
             dpy.sync()
-            os.kill(ff_pid, SIGSTOP)
+            os.killpg(pgid, SIGSTOP)
             wait_for_stop(ff_pid)
             dpy.ungrab_server()
             alive = False
