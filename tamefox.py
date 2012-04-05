41e95e2eddd8a51c5dc80d05058754eae19a0841
Luke Macken <lmacken@redhat.com>
2012-04-05 15:15:00 -0400

    license & copyright

--- tamefox.py
+++ tamefox.py
@@ -1,8 +1,24 @@
-# tamefox.py - Put firefox to sleep when it does not have focus.
+# tamefox.py - Puts firefox & chromium to sleep when they do not have focus.
+#
 # Requirements: python-xlib python-psutil
-# License: GPLv3+
+#
 # Author: Luke Macken <lmacken@redhat.com>
 # Thanks to Jordan Sissel and Adam Jackson for their help
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+# Copyright (C) 2008-2012  Luke Macken <lmacken@redhat.com>
 
 import psutil
 
